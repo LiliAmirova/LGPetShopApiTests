d50e10d7db16a63bb01f8253040c3ac5f173baac
Lilia <lg.amirova79@gmail.com>
2025-07-06 23:54:48 +0300

    Урок 4.Тесты с использованием фикстуры: "Получение информации о питомце по ID", "Обновление информации о питомце (PUT/pet)", "Удаление питомца по ID (DELETE/pet/{petId}"

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+import requests
+
+BASE_URL = "http://5.181.109.28:9090/api/v3"
+
+@pytest.fixture(scope="function")
+def create_pet():
+    """Фикстура для создания питомца, при этом наполнение не проверяем, т.к. это не тест"""
+    payload = {
+        "id": 1,
+        "name": "Buddy",
+        "status": "available"
+    }
+
+    response = requests.post(f"{BASE_URL}/pet", json=payload)
+    assert response.status_code == 200
+    return response.json()  # возвращаемый json будем использовать в тестах
